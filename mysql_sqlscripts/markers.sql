c7d38a03776b7b56e389248f90e99a0936a19f6b
jeffh1cks <jeffreyhicks90@gmail.com>
2023-02-06 05:55:38 -0800

    Created table for markers

---
+++ mysql_sqlscripts/markers.sql
@@ -0,0 +1,10 @@
+CREATE TABLE
+    `markers` (
+        'markerID' int(11) NOT NULL AUTO_INCREMENT,
+        `floorID` int(11) NOT NULL,
+        `buildingID` int(11) NOT NULL,
+        `image` TEXT NULL,
+        `row` varchar(45) DEFAULT NULL,
+        `col` varchar(45) DEFAULT NULL,
+        PRIMARY KEY (`makerID`)
+    ) 
