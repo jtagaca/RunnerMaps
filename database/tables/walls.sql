7ae0f2b15a80049df1a1fbbc64f4172b831bd817
James Tagaca <jtagaca@csub.edu>
2023-02-06 22:30:58 -0800

    fixing non normalized walls sql script

--- database/tables/walls.sql
+++ database/tables/walls.sql
@@ -1,6 +1,5 @@
 CREATE TABLE `wall` (
   `floorID` int(11) NOT NULL,
-  `buildingID` int(11) NOT NULL,
   `row` varchar(45) DEFAULT NULL,
   `col` varchar(45) DEFAULT NULL,
   `wallID` int(11) NOT NULL AUTO_INCREMENT,
