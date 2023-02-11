d1a82755c472cb67c3c54de28ba60c4967ed2135
James Tagaca <jtagaca@csub.edu>
2023-02-11 12:38:10 -0800

    added users.sql create statement

---
+++ database/tables/users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `userType` varchar(45) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `departmentID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`userID`),
+  KEY `foreign_key_of_users_to_department_idx` (`departmentID`),
+  CONSTRAINT `foreign_key_of_users_to_department` FOREIGN KEY (`departmentID`) REFERENCES `department` (`departmentID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
