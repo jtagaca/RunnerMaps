987302a8f6457b8f3c96212fd5f1a8a26f15d629
SummerCodes <jkuo@sandiego.edu>
2023-02-20 22:15:10 -0800

    started login page's basic layout

---
+++ php/index.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>Campus Map</title>
+    </head>
+
+    <body>
+        <h1>Campus Map</h1>
+        
+        <form action="auth/login.php" method="POST">
+            <input type="text" name="username" value="<?=$_SESSION["loginUsername"]?>" placehodler="username"> 
+            <input type="password" name="password" placeholder="*****">
+            <input type="submit" name="login" value="Log In">
+        </form>
+        <br><br>
+        <a href="auth/reset_password.php"> Forgot Password </a>
+        <br> <br>
+        <a href="auth/register.php"> Register an Account </a>
+
+    </body>
+
+</html>
