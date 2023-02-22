cf33821de122a972556dd810dcc0c49ddd158693
Jeff Hicks <jeffreyhicks90@gmail.com>
2023-02-22 11:47:47 -0800

    experimented with using state in React

--- mobile_application/App.js
+++ mobile_application/App.js
@@ -1,25 +1,44 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect } from "react";
+import { Button, Image, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState, useEffect } from "react";
 
-export default function App() {
-  for (let i = 0; i < 10; i++) {
-    debugger;
-    console.log(i);
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      classroom: null,
+    };
   }
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  getDirections = () => {
+    alert(`You entered ${this.state.classroom}`);
+  }
+  // const [classroom, setClassroom] = useState(" ");
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>RUNNER MAPS </Text>
+        <Image source={require('./images/runner.png')} style={{width: 200, height: 200}} />
+        <TextInput
+          style={{height: 40}}
+          placeholder="Class Room Number"
+          onChangeText={newclassroom => this.state.classroom}
+          defaultValue={this.state.classroom}
+        />
+        {/* <Button title="Get Directions" onSubmit={this.getDirections()} /> */}
+        <StatusBar style="auto" />
+      </View>
+      
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#FFFFFF",
     alignItems: "center",
     justifyContent: "center",
+
   },
 });
