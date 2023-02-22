c9d9deb67c1cf6f64444bb9fb061490272b7c4b1
Jeff Hicks <jeffreyhicks90@gmail.com>
2023-02-22 12:03:01 -0800

    reverted back to functional components

--- mobile_application/App.js
+++ mobile_application/App.js
@@ -2,35 +2,28 @@ import { StatusBar } from "expo-status-bar";
 import { Button, Image, StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useState, useEffect } from "react";
 
-export default class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      classroom: null,
-    };
-  }
-  getDirections = () => {
-    alert(`You entered ${this.state.classroom}`);
-  }
-  // const [classroom, setClassroom] = useState(" ");
+export default function App () {
+  const [classroom, setClassroom] = useState("");
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>RUNNER MAPS </Text>
-        <Image source={require('./images/runner.png')} style={{width: 200, height: 200}} />
-        <TextInput
-          style={{height: 40}}
-          placeholder="Class Room Number"
-          onChangeText={newclassroom => this.state.classroom}
-          defaultValue={this.state.classroom}
-        />
-        {/* <Button title="Get Directions" onSubmit={this.getDirections()} /> */}
-        <StatusBar style="auto" />
-      </View>
-      
-    );
+  function getDirections(){
+    alert(`You entered ${classroom}`);
   }
+
+  return (
+    <View style={styles.container}>
+      <Text>RUNNER MAPS </Text>
+      <Image source={require('./images/runner.png')} style={{width: 200, height: 200}} />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Class Room Number"
+        onChangeText={newclassroom => setClassroom(newclassroom)}
+        defaultValue={classroom}
+      />
+      <Button title="Get Directions" onSubmit={getDirections()} />
+      <StatusBar style="auto" />
+    </View>
+    
+  );
 }
 
 const styles = StyleSheet.create({
