45d0565dc5be2720e8be0ec2d6842a4ad04efb08
SummerCodes <jkuo@sandiego.edu>
2023-02-22 23:50:44 -0800

    I tried to combine all sql scripts into the existing all.sql, but it resulted in 2 errors. I want to push this first, then look into potentially modifying them

--- database/all.sql
+++ database/all.sql
@@ -1,4 +1,8 @@
 source tables/buildings.sql;
 source tables/floors.sql;
 source tables/categories.sql;
-
+source tables/users.sql;
+source tables/department.sql;
+source tables/markers.sql;
+source tables/indoorLocations.sql;
+source tables/walls.sql;
