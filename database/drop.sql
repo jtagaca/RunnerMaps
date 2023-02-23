29d04274b4869f0b4a3bc82228d60d82759ffdf3
SummerCodes <jkuo@sandiego.edu>
2023-02-23 00:06:45 -0800

    made a drop.sql due to foreign key constraints when running drop table if exists in all.sql

---
+++ database/drop.sql
@@ -0,0 +1,14 @@
+-- walls.sql named the table wall...
+-- indoorLocations.sql has a diff table name too
+
+-- DROP TABLE IF EXISTS `walls`;
+-- DROP TABLE IF EXISTS `indoorLocations`;
+
+DROP TABLE IF EXISTS `wall`;
+DROP TABLE IF EXISTS `indoor_locations`;
+DROP TABLE IF EXISTS `markers`;
+DROP TABLE IF EXISTS `department`;
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `categories`;
+DROP TABLE IF EXISTS `floors`;
+DROP TABLE IF EXISTS `buildings`;
