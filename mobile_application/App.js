fdae237a48657f0bbe5bd7c31fcc0fd0d242d6b4
Jeff Hicks <jeffreyhicks90@gmail.com>
2023-02-23 15:21:00 -0800

    Updated app to call Map component.

--- mobile_application/App.js
+++ mobile_application/App.js
@@ -1,28 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, Image, StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useState, useEffect } from "react";
+import Maps from './Maps.js';
 
 export default function App () {
   const [classroom, setClassroom] = useState("");
 
-  function getDirections(){
-    alert(`You entered ${classroom}`);
-  }
-
   return (
     <View style={styles.container}>
-      <Text>RUNNER MAPS </Text>
-      <Image source={require('./images/runner.png')} style={{width: 200, height: 200}} />
-      <TextInput
-        style={{height: 40}}
-        placeholder="Class Room Number"
-        onChangeText={newclassroom => setClassroom(newclassroom)}
-        defaultValue={classroom}
-      />
-      <Button title="Get Directions" onSubmit={getDirections()} />
+      <Maps />
       <StatusBar style="auto" />
     </View>
-    
   );
 }
 
