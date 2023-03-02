fe6c25d4ee7dfee033f8f4c944054eba31f60bf7
SummerCodes <jkuo@sandiego.edu>
2023-03-01 21:27:24 -0800

    cleaned up insert_ops.php and commented it

--- php/map/insert_ops.php
+++ php/map/insert_ops.php
@@ -15,15 +15,10 @@ if (isset($_POST["uploadJSON"])) {
         // 
         
             
-        // receiving json content
-        // https://www.geeksforgeeks.org/how-to-receive-json-post-with-php/
-        // https://html.form.guide/php-form/html5-input-type-file/
-        // https://www.php.net/manual/en/features.file-upload.post-method.php
-        // https://www.geeksforgeeks.org/how-to-append-data-in-json-file-through-html-form-using-php/
-        // https://stackoverflow.com/questions/18675353/echo-file-get-contents-filesimagetmp-name-doesnt-do-anything
+        // receiving json content and setting variables
         $inputBuildingName = $_POST["buildingName"];
         $inputFloorNumber = $_POST["floorNumber"];
-
+        
         $inputJSONFile = $_FILES['jsonFile']["tmp_name"];
     
         $fileContent = file_get_contents($inputJSONFile);
@@ -35,35 +30,37 @@ if (isset($_POST["uploadJSON"])) {
         $targetLocations = $decodedJSON->target_locations;
         $markers = $decodedJSON->markers;
         $walls = $decodedJSON->walls;
-
-        // doesBuildingExist("2");
-
         
+        //checks if the building name already exists in the building table
+        //if not, insert it before retrieving its ID
         $buildingExists = doesBuildingAlreadyExist($inputBuildingName);
-        // echo $buildingExists;
 
         if (!$buildingExists) {
             insertIntoBuildings($inputBuildingName, "", "");
         }
         
         $insertBuildingID = getBuildingID($inputBuildingName);
-        // echo $insertBuildingID;
-        // echo $insertBuildingID;
 
+        //checks if the floor plan already exists in the floor table
+        //if not, insert it before retrieving its ID
         $floorExists = doesFloorAlreadyExist($inputBuildingName, $inputFloorNumber);
        
-        // echo "insert: $insertBuildingID.";
-
         if (!$floorExists) {
             insertIntoFloors($inputBuildingName, $inputFloorNumber, $inputGridColumnLength, $inputGridRowLength);
         }
 
         $insertFloorID = getFloorID($inputBuildingName, $inputFloorNumber);
-        // echo $insertFloorID;
+       
+        //could potentially add logic to drop all previous data
+        //associated with this floor before inserting this floor plan
+        //
+        //
+        //
+        //
 
+
+        //insert into the indoor_locations table
         foreach($targetLocations as $rowNumber => $entireRow) {
-            // print_r($rowData);
-            // echo "<br><br>";
             
             $inputRow = $entireRow->row;
             $inputCol = $entireRow->col;
@@ -74,17 +71,10 @@ if (isset($_POST["uploadJSON"])) {
             $inputDescription = "";
 
             insertIntoIndoorLocations($insertFloorID, $inputRow, $inputCol, $inputImage, $inputLatitude, $inputLongitude, $inputName, $inputDescription);
-
-            // print_r($inputName);
-            // if(strlen($inputName) <= 0) {
-            //     echo "null<br>";
-            // }
-            // echo "<br><br>";
         }
 
+        //insert into the markers table
         foreach($markers as $rowNumber => $entireRow) {
-            // print_r($rowData);
-            // echo "<br><br>";
             
             $inputRow = $entireRow->row;
             $inputCol = $entireRow->col;
@@ -93,107 +83,25 @@ if (isset($_POST["uploadJSON"])) {
             $inputImage= $entireRow->image_url;
             
             insertIntoMarkers($insertFloorID, $inputRow, $inputCol, $inputImage, $inputLatitude, $inputLongitude);
-
-            // print_r($inputRow);
-
-            // echo "<br><br>";
         }
 
+        //insert into the wall table
         foreach($walls as $rowNumber => $entireRow) {
-            // print_r($rowData);
-            // echo "<br><br>";
             
             $inputRow = $entireRow->row;
             $inputCol = $entireRow->col;
            
-
             insertIntoWall($insertFloorID, $inputRow, $inputCol);
-
-            // print_r($inputRow);
-            // echo "<br><br>";
         }
-
-
-        //object method:
-        // $decodedJSON = json_decode($fileContent);
-        // print_r($decodedJSON->gridRowLength);
-        // echo "<br><br>";
-
-        // print_r($decodedJSON->gridColumnLength);
-        // echo "<br><br>";
-
-        // print_r($decodedJSON->target_locations);
-        // echo "<br><br>";
-
-        // print_r($decodedJSON->markers);
-        // echo "<br><br>";
-
-        // print_r($decodedJSON->walls);
-        // echo "<br><br>";
-
-        // print_r($decodedJSON->target_locations[0]);
-        // echo "<br><br>";
-        
-        // print_r($decodedJSON->target_locations[0] -> row);
-        // echo "<br><br>";
-
-        
-
-
-        //associative array method:
-        // $decodedJSON = json_decode($fileContent, true);
-
-        // $gridRowLength = $decodedJSON["gridRowLength"];
-        // $gridColumnLength = $decodedJSON["gridColumnLength"];
-        // $targetLocations = $decodedJSON["target_locations"];
-        // $markers = $decodedJSON["markers"]; 
-        // $walls = $decodedJSON["walls"];
-
-        // foreach($targetLocations as $rowNumber => $rowData) {
-        //     foreach($rowData as $key => $value) {
-        //         echo "key: <br>";
-        //         print_r($key);
-        //         echo "<br>";
-        //         echo "value: <br>";
-        //         print_r($value);
-        //         echo "<br>";
-        //         echo "<br>";
-        //     }
-        // }
-
-
-
     }
+
     else {
         echo "if form submission is invalid";
         $_SESSION["error"] = "please submit the form properly <br>";
-
-        // print_r($_POST["buildingName"]);
-        // print_r($_POST["floorNumber"]);
-        // print_r($_POST["jsonFile"]);
     }
     }
     
-    // header("Location: insert_page.php");
+    //redirecting the results back to the form page
+    header("Location: insert_page.php");
     
     ?>
-
-<!-- 
-<?php
-// https://www.php.net/manual/en/features.file-upload.post-method.php
-// $uploaddir = '/var/www/uploads/';
-// $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-
-// echo '<pre>';
-// if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-//     echo "File is valid, and was successfully uploaded.\n";
-// } else {
-//     echo "Possible file upload attack!\n";
-// }
-
-// echo 'Here is some more debugging info:';
-// print_r($_FILES);
-
-// print "</pre>";
-
-?> -->
