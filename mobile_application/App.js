3b4594498138dec4b0e42f346ea264bc8a3ee36b
James Tagaca <jtagaca@csub.edu>
2023-03-05 14:32:49 -0800

    Added Navigation screens to use for navigation between screens

--- mobile_application/App.js
+++ mobile_application/App.js
@@ -1,18 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { View, Button, Text } from "react-native";
-import React, { useState, useEffect } from "react";
-
+import React from "react";
 import { Provider } from "react-redux";
 import store from "./redux_store/store";
-import Test from "./test";
-import tw from "twrnc";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import OutdoorNavigationScreen from "./screens/outdoor_navigation";
+import IndoorNavigationScreen from "./screens/indoor_navigation";
+
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      {/* <Test /> */}
-      <View style={tw`bg-blue-100`}>
-        <Text style={tw.style("text-xl", "text-red-500")}>Hello</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={IndoorNavigationScreen} />
+          <Stack.Screen
+            name="Outdoor_Navigation"
+            component={OutdoorNavigationScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
---
+++ mobile_application/screens/indoor_navigation.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function indoor_navigation() {
+  return (
+    <View>
+      <Text>indoor_navigation</Text>
+    </View>
+  );
+}
