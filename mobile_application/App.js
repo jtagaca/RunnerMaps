fa117a268771d419d50e42da13dc528c687eee26
James Tagaca <jtagaca@csub.edu>
2023-03-04 23:26:04 -0800

    added tailwind css support

--- mobile_application/App.js
+++ mobile_application/App.js
@@ -5,13 +5,13 @@ import React, { useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import store from "./redux_store/store";
 import Test from "./test";
+import tw from "twrnc";
 export default function App() {
   return (
     <Provider store={store}>
       {/* <Test /> */}
-      <View>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+      <View style={tw`bg-blue-100`}>
+        <Text style={tw.style("text-xl", "text-red-500")}>Hello</Text>
       </View>
     </Provider>
   );
