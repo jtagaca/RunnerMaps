a943ad78189f59803bc3818d9115d2b8ec4bf2af
James Tagaca <jtagaca@csub.edu>
2023-03-05 22:50:21 -0800

    added examples to use tailwind

--- mobile_application/screens/indoor_navigation.js
+++ mobile_application/screens/indoor_navigation.js
@@ -1,10 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
-
+import tw from "../tailwind/CustomTailwind";
 export default function IndoorNavigation() {
   return (
     <View>
-      <Text>indoor_navigation</Text>
+      <Text style={tw`jamesbtn`}>indoor_navigation</Text>
     </View>
   );
 }
---
+++ mobile_application/tailwind/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+const { plugin } = require("twrnc");
+module.exports = {
+  content: ["./App.js", "./screens/*.js", "./utilities/*.js"],
+  theme: {
+    extend: {
+      minHeight: {
+        48: "12rem",
+      },
+      colors: {
+        jamescolor: "grey",
+      },
+    },
+  },
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        jamesbtn: {
+          padding: 3,
+          borderRadius: 10,
+          textTransform: `uppercase`,
+          backgroundColor: `jamescolor`,
+        },
+        "resize-repeat": {
+          resizeMode: `repeat`,
+        },
+      });
+    }),
+  ],
+};
