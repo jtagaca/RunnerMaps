42c751754242ed8b106e6aae83a272cd768a74a6
James Tagaca <jtagaca@csub.edu>
2023-03-05 17:10:23 -0800

    Added Icons for navigation

--- mobile_application/App.js
+++ mobile_application/App.js
@@ -4,11 +4,12 @@ import store from "./redux_store/store";
 
 import React, { useState } from "react";
 import { Provider as PaperProvider } from "react-native-paper";
-// import Icon from "react-native-vector-icons/AntDesign";
-import OutdoorNavigationScreen from "./screens/Outdoor_Navigation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import IndoorNavigationScreen from "./screens/Indoor_Navigation";
+import OutdoorNavigationScreen from "./screens/Outdoor_Navigation";
+import Icon from "react-native-vector-icons/FontAwesome";
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -21,10 +22,20 @@ export default function App() {
               <Tab.Screen
                 name="Indoor Navigation"
                 component={IndoorNavigationScreen}
+                options={{
+                  tabBarIcon: ({ color, size }) => (
+                    <Icon name="street-view" color={color} size={size} />
+                  ),
+                }}
               />
               <Tab.Screen
                 name="Outdoor Navigation"
                 component={OutdoorNavigationScreen}
+                options={{
+                  tabBarIcon: ({ color, size }) => (
+                    <Icon name="compass" color={color} size={size} />
+                  ),
+                }}
               />
             </Tab.Navigator>
           </NavigationContainer>
