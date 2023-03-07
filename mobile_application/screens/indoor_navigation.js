a6df86468526591efb16de2ff8d09dd7da365dfa
James Tagaca <jtagaca@csub.edu>
2023-03-07 12:14:54 -0800

    added tailwind brand colors

--- mobile_application/screens/indoor_navigation.js
+++ mobile_application/screens/indoor_navigation.js
@@ -2,9 +2,10 @@ import { View, Text } from "react-native";
 import React from "react";
 import tw from "../tailwind/CustomTailwind";
 export default function IndoorNavigation() {
+  const custom_style = tw`text-center text-white border-b-neutral-400 bg-google-`;
   return (
     <View>
-      <Text style={tw`jamesbtn`}>indoor_navigation</Text>
+      <Text style={custom_style}>indoor_navigation</Text>
     </View>
   );
 }
--- mobile_application/tailwind/tailwind.config.js
+++ mobile_application/tailwind/tailwind.config.js
@@ -26,5 +26,6 @@ module.exports = {
         },
       });
     }),
+    require("tailwindcss-brand-colors"),
   ],
 };
