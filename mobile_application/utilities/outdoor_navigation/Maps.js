483e306184e189a4e540a476e03fd7de79502631
Jeff Hicks <jeffreyhicks90@gmail.com>
2023-03-09 14:37:05 -0800

    Created destination, drew line from curr to dest

--- mobile_application/utilities/outdoor_navigation/Maps.js
+++ mobile_application/utilities/outdoor_navigation/Maps.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import * as Location from 'expo-location';
 import MapViewDirections from 'react-native-maps-directions';
 
 export default function Maps () {
     const [region, setRegion] = useState(null);
-    // const [destination, setDestination] = useState({
-    //     //Icardo Center parking lot
-    //     latitude: 35.346847,
-    //     longitude: -119.102904,
-    // });
+    const [destination, setDestination] = useState({
+        //Icardo Center parking lot
+        latitude: 35.346847,
+        longitude: -119.102904,
+    });
     useEffect(() => {
         (async () => {
         let { status } = await Location.requestForegroundPermissionsAsync();
@@ -27,16 +27,48 @@ export default function Maps () {
           });
         })();
       }, []);
+      const handleGetDirections = async () => {
+        const apiKey = "AIzaSyAJVYqFZxP1cE040AZVjzRHNVAX10jUORI";
+        const location = await Location.getCurrentPositionAsync({});
+        const origin = {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        };
+        const destinationStr = `${destination.latitude},${destination.longitude}`;
+        const directionsUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},
+        ${origin.longitude}&destination=${destinationStr}&key=${apiKey}`;
+        const response = await fetch(directionsUrl);
+        const result = await response.json();
+        const points = MapViewDirections.processPolyline(result.routes[0].overview_polyline.points);
+        setDestination({
+          latitude: result.routes[0].legs[0].end_location.lat,
+          longitude: result.routes[0].legs[0].end_location.lng,
+        });
+        this.map.fitToCoordinates(points.coordinates, {
+          edgePadding: { top: 20, right: 20, bottom: 20, left: 20 },
+          animated: true,
+        });
+
+      }
       return (
         <View style={styles.container}>
         <Text style={styles.titleText}>Runner Maps</Text>
           {region ? (
-            <MapView style={styles.map} region={region}>
-              <Marker coordinate={region} />
+            <MapView style={styles.map} region={region} provider={PROVIDER_GOOGLE}>
+              <Marker coordinate={region} title="You are here" />
+              <Marker coordinate={destination} title="Destination" />
+              <MapViewDirections
+                origin={region}
+                destination={destination}
+                apikey="AIzaSyAJVYqFZxP1cE040AZVjzRHNVAX10jUORI"
+                strokeWidth={3}
+                strokeColor="hotpink"
+              />
             </MapView>
           ) : (
             <Text>Loading...</Text>
           )}
+          <Button style={styles.getDirections}title="Get directions" onPress={handleGetDirections} />
         </View>
       );
 };
@@ -62,4 +94,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '50%',
     },
+    getDirections: {
+      color: "#FFFF00",
+    }
 });
