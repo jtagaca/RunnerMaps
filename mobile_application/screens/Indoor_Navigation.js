2894a103bf452d9be9ae1d8f30fc07d3d4d7e25c
James Tagaca <jtagaca@csub.edu>
2023-03-17 14:30:53 -0700

    adding support for live indoor navigation

--- mobile_application/screens/Indoor_Navigation.js
+++ mobile_application/screens/Indoor_Navigation.js
@@ -14,6 +14,7 @@ import { Switch } from "react-native-switch";
 import * as Location from "expo-location";
 import tw from "../tailwind/CustomTailwind";
 import { Button } from "react-native-paper";
+const haversineDistance = require("geodetic-haversine-distance");
 
 export default function Indoor_Navigation() {
   const dispatch = useDispatch();
@@ -21,12 +22,87 @@ export default function Indoor_Navigation() {
     (state) => state.indoor_navigation_properties.shortest_path_directions
   );
   const swiperRef = useRef(null);
+  const [current_start, setCurrentStart] = useState(null);
+  const [current_end, setCurrentEnd] = useState(null);
+  const [current_distance_between_end, setCurrentDistanceBetweenEnd] =
+    useState(0);
+  const [current_percentage_of_distance, setCurrentPercentageOfDistance] =
+    useState(0);
+  const [current_array_of_checkpoints, setCurrentArrayOfCheckpoints] = useState(
+    []
+  );
+  const [
+    current_difference_index_between_start_and_end,
+    setCurrentDifferenceIndexBetweenStartAndEnd,
+  ] = useState(0);
+  const [current_index_of_checkpoints, setCurrentIndexOfCheckpoints] =
+    useState(0);
+  const [current_render_count, setCurrentRenderCount] = useState(0);
+  const [
+    current_distance_between_start_and_end,
+    setCurrentDistanceBetweenStartAndEnd,
+  ] = useState(0);
+  const [current_multiple_of_percentage, setCurrentMultipleOfPercentage] =
+    useState(1);
 
   const carousel_data = cloneDeep(shortest_path);
-
-  const sorted_shortest_path = carousel_data.sort((a, b) => {
+  let sorted_shortest_path = carousel_data.sort((a, b) => {
     return a.key - b.key;
   });
+  sorted_shortest_path.forEach((element, index) => {
+    element["index"] = index;
+  });
+  useEffect(() => {
+    if (current_render_count === 0) {
+      let temp_array_of_checkpoints = [];
+      let temp_current_start = null;
+      for (let i = 0; i < sorted_shortest_path.length; i++) {
+        if (i == 0) {
+          setCurrentStart(sorted_shortest_path[i]);
+          temp_current_start = sorted_shortest_path[i];
+        } else if (
+          sorted_shortest_path[i].latitude &&
+          sorted_shortest_path[i].longitude
+        ) {
+          temp_array_of_checkpoints.push(sorted_shortest_path[i]);
+        }
+      }
+      setCurrentEnd(temp_array_of_checkpoints[0]);
+      setCurrentIndexOfCheckpoints(0);
+      setCurrentRenderCount(1);
+      setCurrentArrayOfCheckpoints([...temp_array_of_checkpoints]);
+      setCurrentDifferenceIndexBetweenStartAndEnd(
+        parseInt(temp_array_of_checkpoints[0].index) -
+          parseInt(temp_current_start.index)
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    if (sorted_shortest_path) {
+      console.log("sorted_shortest_path", sorted_shortest_path);
+    }
+    if (current_start) {
+      console.log("current_start", current_start);
+    }
+    if (current_end) {
+      console.log("current_end", current_end);
+    }
+    if (current_array_of_checkpoints.length > 0) {
+      console.log("current_array_of_checkpoints", current_array_of_checkpoints);
+    }
+    if (current_index_of_checkpoints) {
+      debugger;
+      console.log("current_index_of_checkpoints", current_index_of_checkpoints);
+    }
+  }, [
+    sorted_shortest_path,
+    current_start,
+    current_end,
+    current_array_of_checkpoints,
+    current_index_of_checkpoints,
+  ]);
+
   const [geolocationProperties, setGeolocationProperties] = useState(null);
   const [current_array_of_pathing, setCurrentArrayOfPathing] = useState([]);
   useEffect(() => {
@@ -44,6 +120,45 @@ export default function Indoor_Navigation() {
         accuracy: Location.Accuracy.Highest,
       });
       setGeolocationProperties(location.coords);
+      if (current_start && current_end) {
+        const harvestine_geolocation_propery_of_current_start = {
+          latitude: parseFloat(current_start.latitude),
+          longitude: parseFloat(current_start.longitude),
+        };
+        const harvestine_geolocation_propery_of_current_end = {
+          latitude: parseFloat(current_end.latitude),
+          longitude: parseFloat(current_end.longitude),
+        };
+        let distance_between_start_and_end = haversineDistance(
+          harvestine_geolocation_propery_of_current_start,
+          harvestine_geolocation_propery_of_current_end
+        );
+        let percentage_multiple =
+          (distance_between_start_and_end /
+            current_difference_index_between_start_and_end /
+            distance_between_start_and_end) *
+          100 *
+          current_multiple_of_percentage;
+
+        let current_distance_between_current_location_and_end =
+          haversineDistance(
+            {
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            },
+            harvestine_geolocation_propery_of_current_end
+          );
+        let current_percentage_traveled =
+          (Math.round(
+            distance_between_start_and_end -
+              current_distance_between_current_location_and_end
+          ) /
+            Math.round(distance_between_start_and_end)) *
+          100;
+
+        // if the percentage is 95% or above then move the current index to +1
+        // and then set the current start to the current end and the current end to the next checkpoint
+      }
     };
 
     updateLocation();
