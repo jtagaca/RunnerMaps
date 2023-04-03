ee263ee40ab17e1c2a525e41b4ef80ee17e26114
James Tagaca <jtagaca@csub.edu>
2023-04-02 18:42:08 -0700

    added loaded once for the loading image to improve performance

--- mobile_application/utilities/Components/LoadingImage.js
+++ mobile_application/utilities/Components/LoadingImage.js
@@ -3,7 +3,15 @@ import { View, Image, ActivityIndicator } from "react-native";
 import tw from "../../tailwind/CustomTailwind";
 
 const LoadingImage = ({ uri }) => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasLoadedOnce, setHasLoadedOnce] = useState(false);
+
+  const handleLoad = () => {
+    setIsLoading(false);
+    if (!hasLoadedOnce) {
+      setHasLoadedOnce(true);
+    }
+  };
 
   return (
     <View style={tw`items-center justify-center relative`}>
@@ -12,9 +20,9 @@ const LoadingImage = ({ uri }) => {
         resizeMode="center"
         style={{ width: "100%", height: 200 }}
         onLoadStart={(e) => setIsLoading(true)}
-        onLoad={(e) => setIsLoading(false)}
+        onLoadEnd={handleLoad}
       />
-      {isLoading && (
+      {isLoading && !hasLoadedOnce && (
         <View style={tw`absolute inset-0 items-center justify-center`}>
           <ActivityIndicator size="large" color="#3B82F6" />
         </View>
