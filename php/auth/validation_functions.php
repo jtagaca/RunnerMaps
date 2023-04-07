86617d96b8fe887931e76747eb940f6d333cb1c1
SummerCodes <jkuo@sandiego.edu>
2023-04-07 12:54:34 -0700

    clarified the use of the validation functions with comments

--- php/auth/validation_functions.php
+++ php/auth/validation_functions.php
@@ -1,5 +1,7 @@
 <?php
 
+// checks if it passes the blank test.
+// returns true if NOT blank
 function blankTest($input) {
     if ((strlen($input) == 0) || ($input == "") || $input == null) {
         return false;
@@ -7,6 +9,8 @@ function blankTest($input) {
     return true;
 }
 
+// checks if it passes the length test.
+// returns true if VALID
 function lengthTest($input, $min, $max) {
     if (strlen($input) < $min || strlen($input) > $max) {
         return false;
